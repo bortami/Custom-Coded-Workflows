297009da0ba3de60f89e712bbf18f176d59cd501
Michelle Tabor <michelle@brdigesstrategies.com>
2022-05-11 17:37:50 -0500

    create repo

---
+++ Renewals/README.md
@@ -0,0 +1,3 @@
+# Set Renewal Custom Automation
+
+This automation sends sets the renewal date on the company record to be the 15th or the 1st, depending on where the original deal close date falls.
---
+++ Renewals/setRenewalDate.js
@@ -0,0 +1,65 @@
+const hubspot = require('@hubspot/api-client');
+
+exports.main = async (event, callback) => {
+
+
+  const hubspotClient = new hubspot.Client({
+    apiKey: process.env.HAPI_KAY
+  });
+
+
+
+  const oldRenewalDate = event.inputFields['renewal_date'];
+  const formattedDateTime = new Date(parseInt(oldRenewalDate)).toLocaleString();
+  const splitDate = formattedDateTime.toString().split("/");
+  const dateObject = {
+   year : splitDate[2],
+   day : splitDate[1],
+   month : splitDate[0]
+}
+  
+  const newDate = (data) => {
+    let newDateObject = {}
+    if (data.day >=16){
+      newDateObject ={
+        year: data.year,
+        day : 1,
+        month: parseInt(data.month) + 1
+      }
+    } else { 
+      newDateObject = {
+        year: data.year,
+        day : 15,
+        month: data.month
+      }  
+  } 
+  return newDateObject
+}
+  const rebuildDate = (param) => {
+    return param.month + "/" + param.day + "/" + param.year
+  }
+  const newRenewalDate = rebuildDate(newDate(dateObject));
+  const convertedToUnix = (new Date(newRenewalDate).getTime() / 1000).toFixed(0);	
+  
+  const dealObj = {
+    id: event.object.objectId,
+    properties: {
+        renewal_date: convertedToUnix * 1000,
+    }
+  };
+  
+	try {
+      const ApiResponse = await hubspotClient.crm.companies.batchApi.update( { inputs:[dealObj] });
+    } catch (err) {
+   console.error(err);
+   throw err;
+}
+
+  callback({
+    outputFields: {
+      newRenewalDate: convertedToUnix,
+    }
+  });
+}
+
+
