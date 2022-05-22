d63c9a6a738bb93614e9c7f4c5fb37905d0b69f4
Michelle Tabor <michelle@brdigesstrategies.com>
2022-05-21 21:30:33 -0500

    Added Data Formatting Codes

---
+++ Data Formatting/lastPageName.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const APIKEY = process.env.HAPI_KAY;
+
+exports.main = async (event, callback) => {
+
+
+  const lastUrlSeen = event.inputFields['hs_analytics_last_url'];
+  const splitURL = lastUrlSeen.split("?");
+  const trimmedURL = splitURL[0].split("/");
+  const slug = trimmedURL[3];
+  const URL = "https://api.hubapi.com/content/api/v2/pages?hapikey=" + APIKEY + "&slug=" + slug;
+  
+  axios.get(URL).then(r => {
+      console.log(r.data.objects[0].title);
+      const pageName = r.data.objects[0].title;
+      callback({
+    	outputFields: {
+      		pageName: pageName,
+    }
+  });
+    }).catch(error => {
+      console.log(error);
+    })
+}
