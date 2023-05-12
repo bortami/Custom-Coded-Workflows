f7d1b8a2f0c824325cf9d691159f18f19e4872a9
Michelle Tabor <michelle.tabor@lynton.com>
2023-05-12 11:12:56 -0500

    mergin

---
+++ Data Formatting/mergeCustom.js
@@ -0,0 +1,69 @@
+const hubspot = require("@hubspot/api-client");
+
+const hubspotClient = new hubspot.Client({
+  accessToken: process.env.TOKEN,
+});
+
+exports.main = async (event, callback) => {
+  const objectId = event.inputFields["hs_object_id"];
+  const email = event.inputFields["contact_email"].trim();
+  const courseID = event.inputFields["issa_course_id"].trim();
+
+  try {
+    
+    const certificateSearchResults = await hubspotClient.crm.objects.searchApi.doSearch("2-7900944", {
+        filterGroups: [
+          {
+            filters: [
+              {
+                propertyName: "contact_email",
+                operator: "EQ",
+                value: email,
+              },
+              {
+                propertyName: "issa_course_id",
+                operator: "EQ",
+                value: courseID,
+              }
+            ],
+          },
+        ],
+        limit: 100,
+      });
+    
+    
+    var objectsToMerge = [];
+    for (let i = 0; i < certificateSearchResults.results.length; i++) {
+      const currentObject = certificateSearchResults.results[i];
+      if (currentObject.properties.issa_course_id == courseID && currentObject.properties.contact_email == email) {
+          objectsToMerge.push({
+            name: currentObject.properties.name,
+            id: currentObject.id
+          });
+        }
+      }
+    
+    console.log(`there are ${objectsToMerge.length} with matching names.`);
+    if(objectsToMerge.length > 0 ){
+    for (let i = 0; i < objectsToMerge.length; i++){
+          // Merge certifications
+          const currentMergeObject = objectsToMerge[i];
+          await hubspotClient.crm.objects.publicObjectApi.merge({
+            primaryObjectId: objectId,
+            objectIdToMerge: currentMergeObject.id,
+          });
+          console.log(
+            currentMergeObject.id,
+            "has been merged into the original object."
+          );
+          break;
+        } 
+    } else {
+      console.log("No objects to merge.");
+    }
+  } catch (err) {
+    console.error("Hit API rate limit. Will retry.", err);
+    // It will automatically retry when the code fails because of a rate limiting error from the HubSpot API.
+    throw err;
+  }
+};
